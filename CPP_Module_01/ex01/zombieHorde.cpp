8f699638793698c244a2395b9b605d09e7b0a1e9
Thibault NOULENS <tnoulens@e1r12p16.clusters.42paris.fr>
2022-11-16 18:27:05 +0100

    CPP 16/11 18.24

--- CPP_Module_01/ex01/zombieHorde.cpp
+++ CPP_Module_01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: tnoulens <tnoulens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 16:16:28 by tnoulens          #+#    #+#             */
-/*   Updated: 2022/11/16 17:23:57 by tnoulens         ###   ########.fr       */
+/*   Updated: 2022/11/16 18:27:00 by tnoulens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ Zombie* zombieHorde( int N, std::string name )
 	Zombie	*z;
 
 	z = new Zombie[N];
-	(void)name;
 	for (int i = 0; i < N; i++)
 		z[i].set_name(name);
 	return (z);
