113a2d389812f9bcce33e3e08747b666ccaee980
Thibault NOULENS <tnoulens@e1r12p16.clusters.42paris.fr>
2022-11-16 19:12:42 +0100

    CPP 16/11 19.12

--- CPP_Module_01/ex01/main.cpp
+++ CPP_Module_01/ex01/main.cpp
@@ -6,23 +6,37 @@
 /*   By: tnoulens <tnoulens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 15:13:56 by tnoulens          #+#    #+#             */
-/*   Updated: 2022/11/16 17:31:27 by tnoulens         ###   ########.fr       */
+/*   Updated: 2022/11/16 19:10:06 by tnoulens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <sstream>
+#include <string>
 #include "colors.h"
 #include "Zombie.hpp"
 
 void	randomChump( std::string name );
 Zombie*	zombieHorde( int N, std::string name );
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	Zombie	*zombheap;
-	int		n = 10;
+	Zombie		*zombheap;
+	int			n;
+	std::string	name;
 
-	zombheap = zombieHorde(n, "Rob");
+	if (argc >= 3)
+	{
+		std::istringstream	is(argv[1]);
+		is >> n;
+		name = argv[2];
+	}
+	else
+	{
+		n = 10;
+		name = "Rob";
+	}
+	zombheap = zombieHorde(n, name);
 	for (int i = 0; i < n; i++)
 	{
 		std::cout << i << " is trying to speak... ";
