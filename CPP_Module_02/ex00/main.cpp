2cf8d6da23b7275c4cc42869dcee8fc103bd774e
Waxxy <72367316@protonmail.com>
2022-11-20 15:57:19 +0100

    02-00

--- CPP_Module_02/ex00/main.cpp
+++ CPP_Module_02/ex00/main.cpp
@@ -6,14 +6,24 @@
 /*   By: waxxy <waxxy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 16:35:07 by waxxy             #+#    #+#             */
-/*   Updated: 2022/11/19 19:55:14 by waxxy            ###   ########.fr       */
+/*   Updated: 2022/11/20 15:47:58 by waxxy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "colors.h"
 #include <iostream>
+#include "Fixed.hpp"
 
-int	main(int argc, char **argv)
+#include <iostream>
+int main(void)
 {
+	Fixed	a;
+	Fixed	b(a);
+	Fixed	c;
+
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
 	return (0);
 }
