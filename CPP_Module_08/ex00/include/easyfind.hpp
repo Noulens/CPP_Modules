497d655c8e88bbab48dcec8bfddf462ac498f92d
Thibault NOULENS <tnoulens@e1r11p8.clusters.42paris.fr>
2022-12-08 17:04:24 +0100

    cpp07

--- CPP_Module_08/ex00/include/easyfind.hpp
+++ CPP_Module_08/ex00/include/easyfind.hpp
@@ -1,26 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iter.hpp                                           :+:      :+:    :+:   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tnoulens <tnoulens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/07 19:42:26 by tnoulens          #+#    #+#             */
-/*   Updated: 2022/12/08 17:03:55 by tnoulens         ###   ########.fr       */
+/*   Created: 2022/12/08 15:16:19 by tnoulens          #+#    #+#             */
+/*   Updated: 2022/12/08 15:16:51 by tnoulens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __ITER__HPP__
-# define __ITER__HPP__
+#ifndef __EASYFIND__HPP__
+# define __EASYFIND__HPP__
 
-# include <iostream>
-
-template <typename T> void iter(T *ok, size_t i, void (*func)(T &))
-{
-    for (size_t j = 0; j < i; ++j)
-    {
-        func(ok[j]);
-    }
-}
 
 #endif
