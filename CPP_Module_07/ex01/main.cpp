2946463029ad0a7eb95930c5f39831c876fa5714
Thibault NOULENS <tnoulens@e1r11p8.clusters.42paris.fr>
2022-12-08 17:11:55 +0100

    cpp07

--- CPP_Module_07/ex01/main.cpp
+++ CPP_Module_07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tnoulens <tnoulens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 19:43:15 by tnoulens          #+#    #+#             */
-/*   Updated: 2022/12/08 17:04:02 by tnoulens         ###   ########.fr       */
+/*   Updated: 2022/12/08 17:06:18 by tnoulens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,26 +48,26 @@ template <typename T> void iter(T *ok, size_t i, void (*func)(T &))
     }
 }
 
-class Awesome
-{
-  public:
-    Awesome( void ) : _n( 42 ) { return; }
-    int get( void ) const { return this->_n; }
-  private:
-    int _n;
-};
+// class Awesome
+// {
+//   public:
+//     Awesome( void ) : _n( 42 ) { return; }
+//     int get( void ) const { return this->_n; }
+//   private:
+//     int _n;
+// };
 
-std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
 
-template< typename T >
-void print( T const & x ) { std::cout << x << std::endl; return; }
+// template< typename T >
+// void print( T const & x ) { std::cout << x << std::endl; return; }
 
-int main() {
-  int tab[] = { 0, 1, 2, 3, 4 };  // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense?
-  Awesome tab2[5];
+// int main() {
+//   int tab[] = { 0, 1, 2, 3, 4 };  // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense?
+//   Awesome tab2[5];
 
-  iter( tab, 5, print<int> );
-  iter( tab2, 5, print<Awesome> );
+//   iter( tab, 5, print<int> );
+//   iter( tab2, 5, print<Awesome> );
 
-  return 0;
-}
+//   return 0;
+// }
