d907e0fe5576b871f9f4a915bb099eef6d83fa1f
tnoulens <thibault.noulens@proton.me>
2022-12-08 20:42:15 +0100

    cpp08

--- CPP_Module_08/ex00/include/easyfind.hpp
+++ CPP_Module_08/ex00/include/easyfind.hpp
@@ -13,5 +13,28 @@
 #ifndef __EASYFIND__HPP__
 # define __EASYFIND__HPP__
 
+# include <algorithm>
+# include <list>
+# include <vector>
+# include <map>
+
+class EasyFindError: public std::exception
+{
+	public:
+		virtual const char *what() const throw()
+		{
+			return("Value not found !\n");
+		}
+};
+
+template <typename T>
+typename T::const_iterator easyfind(const T & target, const int i)
+{
+	typename T::const_iterator	it;
+	it = std::find(target.begin(), target.end(), i);
+	if (it == target.end())
+		throw EasyFindError();
+	return(it);
+}
 
 #endif
--- CPP_Module_08/ex00/main.cpp
+++ CPP_Module_08/ex00/main.cpp
@@ -15,8 +15,39 @@
 #include "colors.h"
 #include <cstdlib>
 
-int	main( void )
+int	main()
 {
-	
+	std::list<int>	lst;
+
+	for (int i = 0; i <= 100; ++i)
+		lst.push_back(i);
+	std::list<int>::const_iterator it;
+	try
+	{
+		it = easyfind(lst, 101);
+		std::cout << *it << std::endl;
+	}
+	catch (EasyFindError &e)
+	{
+		std::cout << e.what();
+	}
+
+	std::vector<int>			lst2(101);
+	std::vector<int>::iterator	it3;
+	int i = 0;
+	for (it3 = lst2.begin(); it3 != lst2.end() ; ++it3)
+	{
+		*it3 = i++;
+	}
+	std::vector<int>::const_iterator it2;
+	try
+	{
+		it2 = easyfind(lst2, 100);
+		std::cout << *it2 << std::endl;
+	}
+	catch (EasyFindError &e)
+	{
+		std::cout << e.what();
+	}
 	return (0);
 }
