e221957c32c904fcee3875606deb59a43d689205
tnoulens <thibault.noulens@proton.me>
2023-03-10 18:50:34 +0100

    09

---
+++ CPP_Module_09/ex00/include/BitcoinExchange.hpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnoulens <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/10 12:09:33 by tnoulens          #+#    #+#             */
+/*   Updated: 2023/03/10 12:09:35 by tnoulens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CPP_MODULES_BITCOINEXCHANGE_HPP
+# define CPP_MODULES_BITCOINEXCHANGE_HPP
+
+
+
+#endif //CPP_MODULES_BITCOINEXCHANGE_HPP
