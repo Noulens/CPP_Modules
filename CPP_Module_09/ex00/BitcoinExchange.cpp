4f8cf7c005bef9177a8f5d2f357f746ce1a3a10b
Waxxy <72367316@protonmail.com>
2023-03-13 17:56:53 +0100

    CPP09

--- CPP_Module_09/ex00/BitcoinExchange.cpp
+++ CPP_Module_09/ex00/BitcoinExchange.cpp
@@ -1,29 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: waxxy <waxxy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/13 17:48:22 by waxxy             #+#    #+#             */
-/*   Updated: 2023/03/13 17:48:23 by waxxy            ###   ########.fr       */
+/*   Created: 2023/03/13 17:48:11 by waxxy             #+#    #+#             */
+/*   Updated: 2023/03/13 17:48:13 by waxxy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CPP_MODULES_BITCOINEXCHANGE_HPP
-# define CPP_MODULES_BITCOINEXCHANGE_HPP
+#include "include/BitcoinExchange.hpp"
 
-class BitcoinExchange
+BitcoinExchange::BitcoinExchange()
 {
-	protected:
-	public:
-	BitcoinExchange();
 
-	BitcoinExchange(const BitcoinExchange &copy);
+}
 
-	~BitcoinExchange();
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &copy)
+{
+	*this = copy;
+}
 
-	BitcoinExchange &operator=(const BitcoinExchange &assign);
-};
+BitcoinExchange::~BitcoinExchange()
+{
 
-#endif
+}
+
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &assign)
+{
+	(void)assign;
+	return (*this);
+}
--- CPP_Module_09/ex00/main.cpp
+++ CPP_Module_09/ex00/main.cpp
@@ -1,29 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: waxxy <waxxy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/13 17:48:22 by waxxy             #+#    #+#             */
-/*   Updated: 2023/03/13 17:48:23 by waxxy            ###   ########.fr       */
+/*   Created: 2023/03/13 17:47:22 by waxxy             #+#    #+#             */
+/*   Updated: 2023/03/13 17:47:23 by waxxy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CPP_MODULES_BITCOINEXCHANGE_HPP
-# define CPP_MODULES_BITCOINEXCHANGE_HPP
+#include "BitcoinExchange.hpp"
 
-class BitcoinExchange
+int	main(int argc, char **argv)
 {
-	protected:
-	public:
-	BitcoinExchange();
-
-	BitcoinExchange(const BitcoinExchange &copy);
-
-	~BitcoinExchange();
-
-	BitcoinExchange &operator=(const BitcoinExchange &assign);
-};
-
-#endif
+	BitcoinExchange	btc;
+	(void)argc;
+	(void)argv;
+	return (0);
+}
